42826fbddf8c26197c319e1c5616704ee16ef2a9
fisal1 <78525562+fisal1@users.noreply.github.com>
2025-11-04 11:05:17 +0530

    Update taskController.js

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -39,9 +39,10 @@ export const deleteTask = async (req, res) => {
     const task = await Task.findById(req.params.id);
     if (!task) return res.status(404).json({ message: 'Task not found' });
     if (task.user.toString() !== req.user._id.toString()) return res.status(403).json({ message: 'Forbidden' });
-    await task.remove();
+    await task.deleteOne();
     res.json({ message: 'Task removed' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
